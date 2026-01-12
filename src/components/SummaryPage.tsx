70d48171ac0b6e512f6429819ce313aee0c4e0e6
katealtonwilliams <kate@altonwilliams.co.uk>
2026-01-12 13:55:42 +0000

    Changin font

--- src/components/SummaryPage.tsx
+++ src/components/SummaryPage.tsx
@@ -3,7 +3,10 @@ import { FC } from "react";
 const Summary: FC = () => {
   return (
     <>
-      <div className="pages left-page">Some words</div>
+      <div className="pages left-page"> 
+        <h2>Your Book Personality</h2>
+
+    </div>
       <div className="pages right-page">Some words</div>
     </>
   );
