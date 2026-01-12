edfa960533607eedef387409fdfc79415bb5c787
katealtonwilliams <kate@altonwilliams.co.uk>
2026-01-12 11:43:38 +0000

    Adding close button that looks like a bookmark

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -43,7 +43,7 @@ const SignModal: React.FC<SignModalProps> = ({ children, onClose }) => {
           style={{ transformOrigin: "center" }}
         >
           {children}
-          <button onClick={onClose}>Close</button>
+          <button className="button" onClick={onClose}></button>
         </motion.div>
       </motion.div>
     </AnimatePresence>
